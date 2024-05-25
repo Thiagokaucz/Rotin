03cf1dc1b27b45b47882961fe4f8e5a1105b029b
Adiel Borges <adielborges31@gmail.com>
2024-05-25 02:40:13 -0300

    Modificar arquivo index.php para atualização de conteúdo

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
+<<<<<<< HEAD
 olha consegui
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+olá
+=======
+
+>>>>>>> 02142b9 (Update index.html)
+=======
+olha consegui
+>>>>>>> 716d8ce (Adicionar novo arquivo index.html)
+=======
+olha consegui
+>>>>>>> 716d8ce30ac2e219fe006edefea8cf55d194dddf
+
+teste teste 
+>>>>>>> dfc231c (Modificar arquivo index.php para atualização de conteúdo)
